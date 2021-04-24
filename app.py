5365193276ff5faace88c538848312eb65dbd2b3
classicallycommie <root@guest.guest>
2021-04-24 22:42:25 +0000

    updated requirements.txt, got app to deploy properly

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 
 from flask import Flask,render_template, jsonify, make_response, request
 import os
-from pyzbar import pyzbar
+#from pyzbar import pyzbar
 from PIL import Image
 
 app = Flask(__name__)
