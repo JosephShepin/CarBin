36d4d9bfde098d1cf550e6334f87b61040407354
Joseph <31457361+jshepin@users.noreply.github.com>
2021-04-24 18:13:10 -0500

    added form

--- app.py
+++ app.py
@@ -15,9 +15,10 @@ def main():
 def results():
     return render_template('results.html')
 
-@app.route('/postData/<vin>', methods=["POST","GET"])
+@app.route('/postData', methods=["POST","GET"])
 def postData(vin):
-    print("the received vin is " + vin)
+    vin = request.form['vin']
+    print("the received vin is 234 " + vin)
     return {"h":"i"}
 
 
