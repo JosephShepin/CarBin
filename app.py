5847f2487d972f91ffa4b30a060936d32a8f1408
Joseph <31457361+jshepin@users.noreply.github.com>
2021-04-24 13:21:41 -0500

    added frontend

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask,render_template, jsonify, make_response
+
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    return render_template('collect.html')
+
+@app.route('/postData', methods=["POST","GET"])
+def postData():
+    return {"h":"i"}
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
+    
