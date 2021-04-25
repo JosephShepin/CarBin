6b1bd1fc4d97f3852d3b5273fed2b8d288e2e7fa
Iyad Hamid <iyad.hamid@gmail.com>
2021-04-25 01:26:55 -0500

    Removed paid api for now

--- car.py
+++ car.py
@@ -207,8 +207,8 @@ class Car:
             url = f'http://api.carsxe.com/platedecoder?key=rnldxnjyx_s9pe9t3ov_kyb2nnr21&plate={vin}&state=MD&format=json'
             r = requests.get(url)
             vin = json.loads(r.text)['vin']
-        #url = 'https://storage.googleapis.com/car-switch/respoonse.json'
-        url =  f'http://api.carsxe.com/specs?key=rnldxnjyx_s9pe9t3ov_kyb2nnr21&vin={vin}'
+        url = 'https://storage.googleapis.com/car-switch/respoonse.json'
+        #url =  f'http://api.carsxe.com/specs?key=rnldxnjyx_s9pe9t3ov_kyb2nnr21&vin={vin}'
         r = requests.get(url)
         return json.loads(r.text)
 
@@ -224,8 +224,8 @@ class Car:
 
     @staticmethod
     def fetch_image(make: str, model: str):
-        url = f"http://api.carsxe.com/images?key=rnldxnjyx_s9pe9t3ov_kyb2nnr21&make={make}&model={model}&angle=front"
-        # return 'https://media.cntraveler.com/photos/57fea9ec8584f8cd20e65f15/16:9/w_1600%2Cc_limit/Aerial-One%26OnlyReethiRah-Maldives-CRHotel.jpg'
+        #url = f"http://api.carsxe.com/images?key=rnldxnjyx_s9pe9t3ov_kyb2nnr21&make={make}&model={model}&angle=front"
+        return 'https://media.cntraveler.com/photos/57fea9ec8584f8cd20e65f15/16:9/w_1600%2Cc_limit/Aerial-One%26OnlyReethiRah-Maldives-CRHotel.jpg'
         r = requests.get(url)
         return json.loads(r.text)["images"][0]["link"]
 
